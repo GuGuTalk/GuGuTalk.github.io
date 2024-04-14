a4674bf3b65788ba2fa6cf5537871d428f3d427d
星绘 <1423237372@qq.com>
2024-04-14 17:58:27 +0800

    新增插入对话、选中删除

--- js/ScreenShot.js
+++ js/ScreenShot.js
@@ -5,15 +5,20 @@ window.onload = function () {
 
 
 }
-const downloadQrcode = () => {
-  var scrollHeight = $('#box').prop("scrollHeight") + 20;
-  domtoimage.toJpeg(document.getElementById('box'), { quality: 1, height: scrollHeight })
-    .then(function (dataUrl) {
-      var link = document.createElement('a');
-      link.download = 'gugutalk.jpeg';
-      link.href = dataUrl;
-      link.click();
-    });
+// const downloadQrcode = () => {
+//   var scrollHeight = $('#box').prop("scrollHeight") + 20;
+//   domtoimage.toJpeg(document.getElementById('box'), { quality: 1, height: scrollHeight })
+//     .then(function (dataUrl) {
+//       console.log(document.getElementsByClassName("iptChoose"));
+//       document.getElementsByClassName("iptChoose").classList.add("chooseNone");
+//       document.getElementsByClassName("iptChooseYHZ").classList.add("chooseNone");
+//       var link = document.createElement('a');
+//       link.download = 'gugutalk.jpeg';
+//       link.href = dataUrl;
+//       link.click();
+//       document.getElementsByClassName("iptChoose").classList.remove("chooseNone");
+//       document.getElementsByClassName("iptChooseYHZ").classList.remove("chooseNone");
+//     });
 
-}
+// }
 
